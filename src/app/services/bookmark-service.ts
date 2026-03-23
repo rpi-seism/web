015d8f2f7d7fe4f29d8736a225415c07196d1d5f
ch3p4ll3 <slinucs@gmail.com>
2026-03-23 20:54:25 +0100

    idk, with ng serve worked :(

--- src/app/services/bookmark-service.ts
+++ src/app/services/bookmark-service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Observable } from 'rxjs';
+import { Bookmark } from '../entities/bookmark';
 
 @Injectable({
   providedIn: 'root',
